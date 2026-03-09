583aed996a2418ff1fe68ce403b1fc4fc350c7d1
0simp <108752705+0simp@users.noreply.github.com>
2026-03-09 20:41:43 +0000

    Update ShrinkEmojis.js

--- ShrinkEmojis.js
+++ ShrinkEmojis.js
@@ -5,7 +5,7 @@
   function apply() {
     const list = document.querySelectorAll('img[alt]')
     list.forEach(item=>{
-      if(item.parentElement.parentElement.className!='fs_12px ov_hidden word-wrap_break-word'&&item.parentElement.parentElement.className){
+      if(item.parentElement.parentElement.className!='fs_12px ov_hidden word-wrap_break-word'&&item.parentElement.parentElement.className&&item.parentElement.className.includes('--emoji-size-large')||item.parentElement.className==='d_inline-block'){
         item.style.setProperty('height','40px')
         item.style.setProperty('width','40px')
       }
