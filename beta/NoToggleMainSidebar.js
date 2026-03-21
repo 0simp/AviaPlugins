826165d76dd702c5252900c688598849d312f957
0simp <108752705+0simp@users.noreply.github.com>
2026-03-21 16:33:00 +0000

    Add files via upload

---
+++ beta/NoToggleMainSidebar.js
@@ -0,0 +1,35 @@
+(function () {
+    if (window.__NO_TOGGLE_MAIN_SIDEBAR__) return;
+    window.__NO_TOGGLE_MAIN_SIDEBAR__ = true;
+
+    function NoToggleMainSidebar() {
+        const original = document.querySelector('div[aria-label="Toggle main sidebar"]')
+        const clone = original.cloneNode(true)
+        const click = original.$$click
+        clone.onclick=click
+        clone.id='togglemainsidebar'
+        const parent = original.parentElement
+        if(!document.getElementById('togglemainsidebar')){
+            parent.removeChild(original)
+            parent.insertBefore(clone,parent.firstChild)
+        }
+    }
+
+    const observer = new MutationObserver(() => {
+        NoToggleMainSidebar();
+    });
+
+    function init() {
+        NoToggleMainSidebar();
+        observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true,
+        });
+    }
+
+    if (document.body) {
+        init();
+    } else {
+        requestAnimationFrame(init);
+    }
+})();
