6c9e0c1b1c911e17e1b17db9fe5c9ef9c2f5858c
0simp <108752705+0simp@users.noreply.github.com>
2026-03-22 22:21:15 +0000

    Update ShrinkGifPanel.js

--- beta/ShrinkGifPanel.js
+++ beta/ShrinkGifPanel.js
@@ -7,6 +7,9 @@
     if(document.getElementsByClassName('w_400px h_400px').item(0)){
         if(document.getElementsByClassName('w_400px h_400px').item(0).children[0].className=='w_100% h_100% us_none d_flex flex-d_column gap_var(--gap-md) ai_stretch ov_hidden p_var(--gap-md)_0 bdr_var(--borderRadius-lg) c_var(--md-sys-color-on-surface) fill_var(--md-sys-color-on-surface) bx-sh_0_0_3px_var(--md-sys-color-shadow) bg_var(--md-sys-color-surface-container)'){
             const gifPanel = document.getElementsByClassName('w_400px h_400px').item(0)
+            gifPanel.style.setProperty('position','fixed')
+            gifPanel.style.setProperty('right','0px')
+            gifPanel.sty.setProperty('bottom','12px')
             gifPanel.style.setProperty('width',`${window.outerWidth-66}px`)
             gifPanel.style.setProperty('height',`${window.outerWidth-66}px`)
             document.getElementsByClassName('min-h_0 d_flex flex-d_column').item(1).children[0].setAttribute('readonly',false)
