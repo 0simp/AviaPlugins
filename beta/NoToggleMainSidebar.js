c18b521cd50f5e9cf7349db0cd26b37401a03344
0simp <108752705+0simp@users.noreply.github.com>
2026-03-24 16:05:35 +0000

    Updated to remove all annoying texts

--- beta/NoToggleMainSidebar.js
+++ beta/NoToggleMainSidebar.js
@@ -1,16 +1,15 @@
 (function () {
   'use strict';
 
-  if (window.__NO_TOGGLE_MAIN_SIDEBAR__) return;
-  window.__NO_TOGGLE_MAIN_SIDEBAR__ = true;
+  if (window.__NO_ANNOYING_TEXTS__) return;
+  window.__NO_ANNOYING_TEXTS__ = true;
 
   const TARGET_CLASS = 'c_white bg_black p_var(--gap-md) bdr_var(--borderRadius-md) lh_0.875rem fs_0.6875rem ls_0.03125rem fw_500';
-  const TARGET_TEXT  = 'Toggle main sidebar';
 
   function removeTooltip() {
     document.querySelectorAll('div.c_white.bg_black').forEach(el => {
-      if (el.className === TARGET_CLASS && el.textContent.trim() === TARGET_TEXT) {
-        el.remove();
+      if (el.className === TARGET_CLASS && el.textContent){
+        el.remove()
       }
     });
   }
