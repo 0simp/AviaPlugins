99768e3c3cf666f655a313ea45ceebfab2687f87
0simp <108752705+0simp@users.noreply.github.com>
2026-03-24 16:54:43 +0000

    Updated to ignore emoji tooltips

--- beta/NoToggleMainSidebar.js
+++ beta/NoToggleMainSidebar.js
@@ -1,15 +1,17 @@
 (function () {
   'use strict';
 
-  if (window.__NO_ANNOYING_TEXTS__) return;
-  window.__NO_ANNOYING_TEXTS__ = true;
+  if (window.__NO_ANNOYING_TOOLTIPS__) return;
+  window.__NO_ANNOYING_TOOLTIPS__ = true;
 
   const TARGET_CLASS = 'c_white bg_black p_var(--gap-md) bdr_var(--borderRadius-md) lh_0.875rem fs_0.6875rem ls_0.03125rem fw_500';
 
   function removeTooltip() {
     document.querySelectorAll('div.c_white.bg_black').forEach(el => {
       if (el.className === TARGET_CLASS && el.textContent){
-        el.remove()
+        if(!el.firstChild?.firstChild?.outerHTML?.includes('emoji-size')){
+          el.remove()
+        }
       }
     });
   }
