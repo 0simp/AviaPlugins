36c512f8a067443c40498d5500a05ee4bc9596ee
0simp <108752705+0simp@users.noreply.github.com>
2026-03-30 20:31:16 +0100

    Add files via upload

---
+++ GifFix.js
@@ -0,0 +1,31 @@
+(function () {
+  if (window.__FART_CLICK__) return;
+  window.__FART_CLICK__ = true;
+
+  function GifFix() {
+    const images = document.querySelectorAll('img')
+    images.forEach(image=>{
+        if(!image.src.includes('original')&&!image.src.includes('default_avatar')&&!image.src.includes('cdn.jsdelivr.net')){
+            image.src=image.src+'/original'
+        }
+    })
+  }
+
+  const observer = new MutationObserver(() => {
+    GifFix();
+  });
+
+  function init() {
+    GifFix();
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+  }
+
+  if (document.body) {
+    init();
+  } else {
+    requestAnimationFrame(init);
+  }
+})();
