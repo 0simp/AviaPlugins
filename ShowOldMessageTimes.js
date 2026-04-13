df01b1a5204b10ced8a92acd06b4cd9bee8560fb
0simp <0simp@noreply.codeberg.org>
2026-04-13 20:12:49 +0200

    Make timezone always accurate

--- ShowOldMessageTimes.js
+++ ShowOldMessageTimes.js
@@ -5,8 +5,12 @@
   function showOldMessageTimes() {
     document.querySelectorAll('time').forEach(element=>{
         if(!element.parentElement.className&&!element.textContent.includes('at')){
-            const datetime = element.getAttribute('datetime')
-            element.textContent = element.textContent+` at ${datetime.substring(datetime.indexOf('T')+1,datetime.lastIndexOf(':'))}`
+            let timestamp = element.outerHTML;
+            timestamp = timestamp.substring(timestamp?.indexOf('"')+1,timestamp?.lastIndexOf('"'))
+            timestamp = Math.floor(new Date(timestamp).getTime())
+            const date = new Date(timestamp)
+            const time = date.toString().substring(16,date.toString().lastIndexOf(':'))
+            element.textContent = element.textContent+` at ${time}`
         }
     })
   }
