a92bfc4a936239c8ff1b1adee8c024356f064d4a
0simp <0simp@noreply.codeberg.org>
2026-04-16 19:58:55 +0200

    Fix slowmode box appearing in server settings

--- Slowmode.js
+++ Slowmode.js
@@ -10,7 +10,7 @@
     clone.setAttribute('maxlength','5')
     clone.setAttribute('placeholder','0-21600')
     clone.setAttribute('name','slowmode')
-    if(!document.querySelector('mdui-text-field[name=\'slowmode\']')){
+    if(!document.querySelector('mdui-text-field[name=\'slowmode\']')&&document.querySelector('path[d=\'M18.944 11.112C18.507 7.67 15.56 5 12 5 9.244 5 6.85 6.611 5.757 9.15 3.609 9.792 2 11.82 2 14c0 2.757 2.243 5 5 5h11c2.206 0 4-1.794 4-4a4.01 4.01 0 0 0-3.056-3.888z\']')){
         descriptionbox.parentElement.insertBefore(clone,descriptionbox.nextSibling)
 
         const originalFetch = window.fetch.bind(window);
