ab0050602da1a00f319491d357c55f423d88e275
0simp <0simp@noreply.codeberg.org>
2026-05-02 20:31:02 +0200

    Fix showing duplicate times on tooltips

--- ShowOldMessageTimes.js
+++ ShowOldMessageTimes.js
@@ -4,12 +4,12 @@
 
   function showOldMessageTimes() {
     document.querySelectorAll('time').forEach(element=>{
-        if(!element.parentElement.className&&!element.textContent.includes('at')){
-            let timestamp = element.outerHTML;
-            timestamp = timestamp.substring(timestamp?.indexOf('"')+1,timestamp?.lastIndexOf('"'))
-            timestamp = Math.floor(new Date(timestamp).getTime())
-            const date = new Date(timestamp)
-            const time = date.toString().substring(16,date.toString().lastIndexOf(':'))
+      let timestamp = element.outerHTML;
+      timestamp = timestamp.substring(timestamp?.indexOf('"')+1,timestamp?.lastIndexOf('"'))
+      timestamp = Math.floor(new Date(timestamp).getTime())
+      const date = new Date(timestamp)
+      const time = date.toString().substring(16,date.toString().lastIndexOf(':'))
+        if(!element.parentElement.className&&!element.textContent.includes(`${time}`)){
             element.textContent = element.textContent+` at ${time}`
         }
     })
