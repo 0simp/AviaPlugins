3951d477bd92bbb78d6ed7d66a2cf993f32f23d7
0simp <0simp@noreply.codeberg.org>
2026-05-02 20:42:48 +0200

    don't hide default badge tooltips in big user popup+hide sent at tooltips if small user popup open

--- NoAnnoyingTooltips.js
+++ NoAnnoyingTooltips.js
@@ -9,7 +9,10 @@
   function removeTooltip() {
     document.querySelectorAll('div.c_white.bg_black').forEach(el => {
       if (el.className === TARGET_CLASS && el.textContent){
-        if(!el.firstChild?.firstChild?.outerHTML?.includes('emoji-size')&&!el.style?.color&&!el.firstChild?.style?.color&&el.parentElement?.parentElement?.parentElement?.firstChild?.firstChild?.firstChild?.className!=='will-change_transform scr-bar-w_none [&::-webkit-scrollbar]:d_none ov-y_scroll c_var(--md-sys-color-on-surface) bg_var(--md-sys-color-surface-container-high) bx-sh_0_0_3px_var(--md-sys-color-shadow) w_340px h_400px bdr_var(--borderRadius-xl)'){
+        if(!el.firstChild?.firstChild?.outerHTML?.includes('emoji-size')&&!el.style?.color&&!el.firstChild?.style?.color //make emoji and avia badge tooltips show
+        &&el.parentElement?.parentElement?.parentElement?.firstChild?.firstChild?.firstChild?.className!=='will-change_transform scr-bar-w_none [&::-webkit-scrollbar]:d_none ov-y_scroll c_var(--md-sys-color-on-surface) bg_var(--md-sys-color-surface-container-high) bx-sh_0_0_3px_var(--md-sys-color-shadow) w_340px h_400px bdr_var(--borderRadius-xl)' //make default badge tooltips show (in small user popup)
+        &&el.parentElement?.parentElement?.parentElement?.parentElement?.lastChild?.firstChild?.firstChild?.firstChild.className!=='p_24px min-w_280px max-w_560px bdr_28px d_flex flex-d_column c_var(--md-sys-color-on-surface) bg_var(--md-sys-color-surface-container-high)' //make default badge tooltips show (in big user popup)
+        ||el.parentElement?.parentElement?.firstChild?.firstChild?.firstChild?.firstChild?.lastChild.tagName=='TIME'){ //make sent at tooltips not show when small user popup is open
           el.remove()
         }
       }
