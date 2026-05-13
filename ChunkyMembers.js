cfa0da6456626967340eae04428fb2cc1464bd76
0simp <0simp@noreply.codeberg.org>
2026-05-14 00:30:40 +0200

    fix background becoming more and more zoomed in each time a mutation occurs

--- ChunkyMembers.js
+++ ChunkyMembers.js
@@ -4,7 +4,8 @@
 
   function chunkyMembers() {
     const memberlist = document.getElementsByClassName('will-change_transform scr-bar-c_var(--md-sys-color-primary)_transparent ov-y_auto ov-x_hidden ov_hidden! scr-bar-g_stable flex-sh_0 w_var(--layout-width-channel-sidebar) bdr_var(--borderRadius-lg)').item(0)
-    if(memberlist&&memberlist.style.width!=`${memberlist.clientWidth+memberlist.previousSibling.clientWidth}px`){
+    if(!memberlist) return;
+    if(!memberlist.style.width||Number(memberlist.style.width.replace('px',''))<memberlist.previousSibling.clientWidth){
       memberlist.style.width = `${memberlist.clientWidth+memberlist.previousSibling.clientWidth}px`
     }
   }
