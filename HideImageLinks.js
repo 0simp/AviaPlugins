725ebd011e99a4b5393cb81cfa2acd5d43124fc3
0simp <0simp@noreply.codeberg.org>
2026-06-18 11:54:26 +0200

    add reply support

--- HideImageLinks.js
+++ HideImageLinks.js
@@ -5,6 +5,7 @@
   function hideImageLinks() {
     const messages = document.getElementsByClassName('group pos_relative d_flex flex-d_column p_2px_0 bg_transparent bdr_var(--borderRadius-md) min-h_1em trs_background-color_var(--transitions-fast) [&_a:hover]:td_underline [&:hover_.Toolbar]:d_flex mt_var(--message-group-spacing)! [&:hover]:bg_var(--md-sys-color-surface-container) c_var(--md-sys-color-on-surface)')
     const messages2 = document.getElementsByClassName('group pos_relative d_flex flex-d_column p_2px_0 bg_transparent bdr_var(--borderRadius-md) min-h_1em trs_background-color_var(--transitions-fast) [&_a:hover]:td_underline [&:hover_.Toolbar]:d_flex mt_0 [&:hover]:bg_var(--md-sys-color-surface-container) c_var(--md-sys-color-on-surface)')
+    const replies = document.getElementsByClassName('group pos_relative d_flex flex-d_column p_2px_0 bg_var(--md-sys-color-primary-container) bdr_var(--borderRadius-md) min-h_1em trs_background-color_var(--transitions-fast) [&_a:hover]:td_underline [&:hover_.Toolbar]:d_flex mt_var(--message-group-spacing)! [&:hover]:bg_var(--md-sys-color-surface-container) c_var(--md-sys-color-on-surface)')
 
     for(const message of messages){
         const linkElement = message.children[1]?.children[1]?.children[1]?.children[0]?.children[0]?.children[0]?.children[0]
@@ -27,6 +28,18 @@
         if((link==text)&&image?.tagName=='IMG'&&!linkElement.alt){
             linkElement.style.display='none'
         }
+    }
+
+    for(const reply of replies){
+      const linkElement = reply.querySelector(`a[title][class='cursor_pointer c_var(--md-sys-color-primary)!']`)
+      const link = linkElement?.title
+      const textElement = reply.querySelector(`p[class='[&>code]:flex-sh_0 [&>code]:p_1px_4px [&>code]:bdr_var(--borderRadius-md) [&>code]:c_#c9d1d9 [&>code]:bg_#0d1117']`)
+      const text = textElement?.textContent
+      const image = reply.querySelector(`img[class='cursor_pointer']`)
+
+      if(link==text&&image?.tagName=='IMG'&&!linkElement.alt){
+        linkElement.style.display='none'
+      }
     }
   }
 
