d73465ee0df37c0ac45a36ec83fd93daf9db82fa
0simp <0simp@noreply.codeberg.org>
2026-06-20 19:59:37 +0200

    Fix github links getting hidden+fix replies being broken on web 0.9.1

--- HideImageLinks.js
+++ HideImageLinks.js
@@ -5,7 +5,7 @@
   function hideImageLinks() {
     const messages = document.getElementsByClassName('group pos_relative d_flex flex-d_column p_2px_0 bg_transparent bdr_var(--borderRadius-md) min-h_1em trs_background-color_var(--transitions-fast) [&_a:hover]:td_underline [&:hover_.Toolbar]:d_flex mt_var(--message-group-spacing)! [&:hover]:bg_var(--md-sys-color-surface-container) c_var(--md-sys-color-on-surface)')
     const messages2 = document.getElementsByClassName('group pos_relative d_flex flex-d_column p_2px_0 bg_transparent bdr_var(--borderRadius-md) min-h_1em trs_background-color_var(--transitions-fast) [&_a:hover]:td_underline [&:hover_.Toolbar]:d_flex mt_0 [&:hover]:bg_var(--md-sys-color-surface-container) c_var(--md-sys-color-on-surface)')
-    const replies = document.getElementsByClassName('group pos_relative d_flex flex-d_column p_2px_0 bg_var(--md-sys-color-primary-container) bdr_var(--borderRadius-md) min-h_1em trs_background-color_var(--transitions-fast) [&_a:hover]:td_underline [&:hover_.Toolbar]:d_flex mt_var(--message-group-spacing)! [&:hover]:bg_var(--md-sys-color-surface-container) c_var(--md-sys-color-on-surface)')
+    const replies = document.getElementsByClassName('group pos_relative d_flex flex-d_column p_2px_0 bg_transparent bdr_var(--borderRadius-md) min-h_1em trs_background-color_var(--transitions-fast) [&_a:hover]:td_underline [&:hover_.Toolbar]:d_flex mt_var(--message-group-spacing)! [&:hover]:bg_var(--md-sys-color-surface-container) c_var(--md-sys-color-on-surface)')
 
     for(const message of messages){
         const linkElement = message.children[1]?.children[1]?.children[1]?.children[0]?.children[0]?.children[0]?.children[0]
@@ -13,9 +13,12 @@
         const textElement = message.children[1]?.children[1]?.children[1]?.children[0]?.children[0]
         const text = textElement?.textContent
         const image = message.children[1]?.children[1]?.querySelector(`img[class=cursor_pointer]`)
+        const embed = message.children[1]?.children[1]?.querySelector(`div[class='d_flex flex-d_column flex-g_1 m_0 ai_initial jc_initial min-w_0 gap_var(--gap-md)']`)
 
-        if((link==text)&&image?.tagName=='IMG'&&!linkElement.alt){
-            linkElement.style.display='none'
+        if((linkElement&&link==text)&&image?.tagName=='IMG'&&!linkElement.alt){
+            if(!embed||(embed&&![...embed.querySelectorAll(`img[class='cursor_pointer']`)].filter(img=>img.src==image?.src))){
+              linkElement.style.display='none'
+            }
         }
     }
 
@@ -24,9 +27,12 @@
         const link = linkElement?.title
         const text = message.children[1]?.children[1]?.children[0]?.children[0]?.textContent
         const image = message.children[1]?.children[1]?.querySelector(`img[class=cursor_pointer]`)
+        const embed = message.children[1]?.children[1]?.querySelector(`div[class='d_flex flex-d_column flex-g_1 m_0 ai_initial jc_initial min-w_0 gap_var(--gap-md)']`)
 
-        if((link==text)&&image?.tagName=='IMG'&&!linkElement.alt){
-            linkElement.style.display='none'
+        if((linkElement&&link==text)&&image?.tagName=='IMG'&&!linkElement.alt){
+            if(!embed||(embed&&![...embed.querySelectorAll(`img[class='cursor_pointer']`)].filter(img=>img.src==image?.src))){
+              linkElement.style.display='none'
+            }
         }
     }
 
@@ -36,9 +42,12 @@
       const textElement = reply.querySelector(`p[class='[&>code]:flex-sh_0 [&>code]:p_1px_4px [&>code]:bdr_var(--borderRadius-md) [&>code]:c_#c9d1d9 [&>code]:bg_#0d1117']`)
       const text = textElement?.textContent
       const image = reply.querySelector(`img[class='cursor_pointer']`)
+      const embed = reply.querySelector(`div[class='d_flex flex-d_column flex-g_1 m_0 ai_initial jc_initial min-w_0 gap_var(--gap-md)']`)
 
-      if(link==text&&image?.tagName=='IMG'&&!linkElement.alt){
-        linkElement.style.display='none'
+      if(linkElement&&link==text&&image?.tagName=='IMG'&&!linkElement.alt){
+          if(!embed||(embed&&![...embed.querySelectorAll(`img[class='cursor_pointer']`)].filter(img=>img.src==image?.src))){
+            linkElement.style.display='none'
+          }
       }
     }
   }
