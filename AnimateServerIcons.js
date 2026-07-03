f3ccda38cd5de09e0fc102af816e8786085f6100
0simp <0simp@noreply.codeberg.org>
2026-07-03 19:40:45 +0200

    Fix icons in invites not animating

--- AnimateServerIcons.js
+++ AnimateServerIcons.js
@@ -14,7 +14,7 @@
         }
     })
 
-    const invites = document.querySelectorAll(`div[class='d_flex ai_center w_320px h_64px gap_var(--gap-md) p_var(--gap-md) bdr_var(--borderRadius-md) c_var(--md-sys-color-on-secondary-container) bg_var(--md-sys-color-secondary-container)']`)
+    const invites = document.querySelectorAll(`div[class='d_flex ai_center max-w_320px h_64px gap_var(--gap-md) p_var(--gap-md) bdr_var(--borderRadius-md) c_var(--md-sys-color-on-secondary-container) bg_var(--md-sys-color-secondary-container)']`)
 
     invites.forEach(invite=>{
         const icon = invite.querySelector(`img`)
