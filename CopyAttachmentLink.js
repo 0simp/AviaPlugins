396118884ac9fabc85cdec54951d2414fef228bc
0simp <0simp@noreply.codeberg.org>
2026-07-29 19:52:23 +0200

    Improve compatibility with CopyFileContents

--- CopyAttachmentLink.js
+++ CopyAttachmentLink.js
@@ -1,6 +1,6 @@
 /*
   @UPDATEURL: https://codeberg.org/0simp/AviaPlugins/raw/branch/main/CopyAttachmentLink.js
-  @VERSION: 1.0
+  @VERSION: 1.1
 */
 
 (function () {
@@ -27,17 +27,25 @@
     savefilebutton.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M19 9h-4V3H9v6H5l7 7zm-8 2V5h2v6h1.17L12 13.17 9.83 11zm-6 7h14v2H5z"></path></svg><span class="fzuZQP hZMpMu VJSWr ieGCzW jRYfTR">Save file</span>`
     download.appendChild(savefilebutton)
 
-    replybutton.parentElement.insertBefore(divider,replybutton)
-    replybutton.parentElement.insertBefore(download,divider)
-    replybutton.parentElement.insertBefore(copylinkbutton,download)
-    replybutton.parentElement.insertBefore(openfilebutton,copylinkbutton)
+    const copytextbutton = document.querySelector(`a:has(svg>path[d='M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z'])`)
+    if(copytextbutton){
+      replybutton.parentElement.insertBefore(copylinkbutton,copytextbutton)
+      replybutton.parentElement.insertBefore(openfilebutton,copylinkbutton)
+      replybutton.parentElement.insertBefore(download,copytextbutton.nextSibling)
+    }else{
+      replybutton.parentElement.insertBefore(divider,replybutton)
+      replybutton.parentElement.insertBefore(download,divider)
+      replybutton.parentElement.insertBefore(copylinkbutton,download)
+      replybutton.parentElement.insertBefore(openfilebutton,copylinkbutton)
+    }
   }
 
   function apply() {
     const textfilesvgs = document.querySelectorAll(`path[d='M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zM9.998 14.768H8.895v3.274h-.917v-3.274H6.893V14h3.105v.768zm2.725 3.274-.365-.731c-.15-.282-.246-.492-.359-.726h-.013c-.083.233-.185.443-.312.726l-.335.731h-1.045l1.171-2.045L10.336 14h1.05l.354.738c.121.245.21.443.306.671h.013c.096-.258.174-.438.276-.671l.341-.738h1.043l-1.139 1.973 1.198 2.069h-1.055zm4.384-3.274h-1.104v3.274h-.917v-3.274h-1.085V14h3.105v.768zM14 9h-1V4l5 5h-4z']`)
     textfilesvgs.forEach(svg=>{
       if(svg.parentElement.parentElement.querySelector(`[aria-label='Copy attachment link']`)) return;
-      const copyLinkButton = svg.parentElement.parentElement.lastChild.firstChild.cloneNode(true)
+      const downloadbutton = svg.parentElement.parentElement.querySelector(`a[href]`).firstChild
+      const copyLinkButton = downloadbutton.cloneNode(true)
       copyLinkButton.lastChild.textContent='link'
       copyLinkButton.ariaLabel='Copy attachment link'
       copyLinkButton.addEventListener('click',()=>{
@@ -57,14 +65,17 @@
                 pointerEvents: "none",
                 color:"white"
             });
+
+            navigator.clipboard.writeText(downloadbutton.parentElement.href).catch(err=>{
+              toast.textContent='Failed to copy'
+              toast.style.color='red'
+            });
             copyLinkButton.parentElement.appendChild(toast);
             requestAnimationFrame(() => toast.style.opacity = "1");
             setTimeout(() => {
                 toast.style.opacity = "0";
                 setTimeout(() => toast.remove(), 200);
             }, 2000);
-
-            navigator.clipboard.writeText(copyLinkButton.previousSibling.href)
         });
         svg.parentElement.parentElement.appendChild(copyLinkButton)
         svg.parentElement.parentElement.parentElement.addEventListener('contextmenu',(e)=>{
