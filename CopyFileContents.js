a7626298a6f1425e889423882c5d32eb16efac5b
0simp <0simp@noreply.codeberg.org>
2026-07-29 19:51:48 +0200

    Update to work on latest stoat version

--- CopyFileContents.js
+++ CopyFileContents.js
@@ -1,90 +1,49 @@
+/*
+  @UPDATEURL: https://codeberg.org/0simp/AviaPlugins/raw/branch/main/CopyFileContents.js
+  @VERSION: 1.0
+*/
+
 (function () {
   if (window.__COPY_FILE_CONTENTS__) return;
   window.__COPY_FILE_CONTENTS__ = true;
 
-  function addContextMenuButton(e){
-    setTimeout(() => {
-      const contextmenu = document.getElementsByClassName('d_flex flex-d_column p_var(--gap-md)_0 ov_hidden bdr_var(--borderRadius-xs) bg_var(--md-sys-color-surface-container) c_var(--md-sys-color-on-surface) fill_var(--md-sys-color-on-surface) bx-sh_0_0_3px_var(--md-sys-color-shadow) us_none').item(0)
-      if(!contextmenu) return;
+  function addContextMenuButton(replybutton,svg){
+    if(!replybutton||!svg) return;
+    document.body.style.overflow='hidden'
+    const divider = [...replybutton.parentElement.children].find(e=>!e.firstChild).cloneNode()
 
-      const copyfilecontent = document.createElement('a')
-      copyfilecontent.className='d_flex gap_var(--gap-md) ai_center p_var(--gap-md)_var(--gap-lg) [&:hover]:bg_color-mix(in_srgb,_var(--md-sys-color-on-surface)_8%,_transparent) [&_span]:flex-g_1 [&_span]:mt_1px cursor_pointer tt_capitalize'
+    const copytextbutton = replybutton.cloneNode(true)
+    copytextbutton.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="#e3e3e3"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg><span class="fzuZQP hZMpMu VJSWr ieGCzW jRYfTR">Copy file contents</span>`
+    copytextbutton.onclick = function(){
+      if(svg.parentElement.parentElement.nextSibling.querySelector('button')) return;
+      navigator.clipboard.writeText(`${svg.parentElement.parentElement.nextSibling.textContent}`).catch(err=>{});
+    }
 
-      const mdripple = document.createElement('md-ripple')
-      mdripple.ariaHidden=true
-
-      const icon = document.createElement('span')
-      icon.className='material-symbols-outlined fs_inherit fw_undefined!'
-      icon.style='display: block; font-size:16px; font-variation-settings: &quot;FILL&quot; 0, &quot;wght&quot; 400, &quot;GRAD&quot; 0;'
-      icon.textContent='content_copy'
-
-      const text = document.createElement('span')
-      text.className='lh_1.25rem fs_0.875rem ls_0.015625rem fw_400'
-      text.textContent='Copy file content'
-
-      copyfilecontent.onclick = function(){
-        let attachment;
-        let href;
-         if(e.target.tagName=='CODE'){
-          attachment = e.target.parentElement.parentElement.parentElement
-          href=attachment.firstChild.children[2].href
-        }else{
-          attachment = e.target.parentElement.parentElement.parentElement.parentElement.parentElement
-          href=attachment.firstChild.children[2].href
-        }
-
-        if(href.includes('/channel')){
-          attachment = e.target.parentElement.parentElement.parentElement.parentElement.parentElement
-        }
-
-        navigator.clipboard.writeText(attachment.children[1].textContent).catch(e=>{
-
-        });
-      }
-
-      copyfilecontent.appendChild(mdripple)
-      copyfilecontent.appendChild(icon)
-      copyfilecontent.appendChild(text)
-
-      const savefilesvg = contextmenu.querySelector(`path[d='M19 9h-4V3H9v6H5l7 7zm-8 2V5h2v6h1.17L12 13.17 9.83 11zm-6 7h14v2H5z']`)
-      if(!savefilesvg){
-        const divider = document.createElement('div')
-        divider.className='h_1px m_var(--gap-sm)_0 bg_var(--md-sys-color-outline-variant)'
-
-        contextmenu.insertBefore(divider,contextmenu.firstChild)
-        contextmenu.insertBefore(copyfilecontent,contextmenu.firstChild)
-      }else{
-        contextmenu.insertBefore(copyfilecontent,contextmenu.children[2])
-      }
-    }, 100);
+    const copylinkbutton = document.querySelector(`a:has(svg>path[d='M17 7h-4v2h4c1.65 0 3 1.35 3 3s-1.35 3-3 3h-4v2h4c2.76 0 5-2.24 5-5s-2.24-5-5-5m-6 8H7c-1.65 0-3-1.35-3-3s1.35-3 3-3h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4zm-3-4h8v2H8z'])`)
+    if(copylinkbutton){
+      replybutton.parentElement.insertBefore(copytextbutton,copylinkbutton.nextSibling)
+    }else{
+      replybutton.parentElement.insertBefore(divider,replybutton)
+      replybutton.parentElement.insertBefore(copytextbutton,divider)
+    }
   }
 
   function copyFileContents() {
-    const elements = [... document.querySelectorAll('div[class="d_flex flex-d_column flex-g_initial m_0 ai_initial jc_initial p_var(--gap-md) bdr_var(--borderRadius-md) c_var(--md-sys-color-inverse-on-surface) bg_var(--md-sys-color-inverse-surface) gap_var(--gap-md)"]')]
-    .filter(e=>!e.querySelector(`path[d='M20 12v-1.707c0-4.442-3.479-8.161-7.755-8.29-2.204-.051-4.251.736-5.816 2.256A7.933 7.933 0 0 0 4 10v2c-1.103 0-2 .897-2 2v4c0 1.103.897 2 2 2h2V10a5.95 5.95 0 0 1 1.821-4.306 5.977 5.977 0 0 1 4.363-1.691C15.392 4.099 18 6.921 18 10.293V20h2c1.103 0 2-.897 2-2v-4c0-1.103-.897-2-2-2z']`))
-    elements.forEach(element=>{
-        const copyfilecontent = document.createElement('button')
-        copyfilecontent.className='ov-wrap_anywhere lh_1.25rem fs_0.875rem ls_0.015625rem fw_500 pos_relative asp_1/1 flex-sh_0 d_flex ai_center jc_center ff_inherit cursor_pointer bd_none trs_var(--transitions-fast)_all c_var(--colour) fill_var(--colour) --colour_var(--md-sys-color-on-surface-variant) bdr_var(--borderRadius-full) h_40px px_8px'
-
-        const mdripple = document.createElement('md-ripple')
-        mdripple.ariaHidden=true
-
-        const span = document.createElement('span')
-        span.className='material-symbols-outlined fs_inherit fw_undefined!'
-        span.style='display: block; font-variation-settings: &quot;FILL&quot; 0, &quot;wght&quot; 400, &quot;GRAD&quot; 0;'
-        span.textContent='content_copy'
-
-        copyfilecontent.appendChild(mdripple)
-        copyfilecontent.appendChild(span)
-
-        copyfilecontent.onclick = function(e){
-            const toast = document.createElement('div')
+    const textfilesvgs = document.querySelectorAll(`path[d='M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zM9.998 14.768H8.895v3.274h-.917v-3.274H6.893V14h3.105v.768zm2.725 3.274-.365-.731c-.15-.282-.246-.492-.359-.726h-.013c-.083.233-.185.443-.312.726l-.335.731h-1.045l1.171-2.045L10.336 14h1.05l.354.738c.121.245.21.443.306.671h.013c.096-.258.174-.438.276-.671l.341-.738h1.043l-1.139 1.973 1.198 2.069h-1.055zm4.384-3.274h-1.104v3.274h-.917v-3.274h-1.085V14h3.105v.768zM14 9h-1V4l5 5h-4z']`)
+    textfilesvgs.forEach(svg=>{
+       if(svg.parentElement.parentElement.querySelector(`[aria-label='Copy file contents']`)) return;
+        const downloadbutton = svg.parentElement.parentElement.querySelector(`a[href]`).firstChild
+       const copytextbutton = downloadbutton.cloneNode(true)
+       copytextbutton.lastChild.textContent='content_copy'
+       copytextbutton.ariaLabel='Copy file contents'
+       copytextbutton.onclick = function(e){
+        if(svg.parentElement.parentElement.nextSibling.querySelector('button')) return;
+        const toast = document.createElement('div')
             toast.textContent='Copied!'
             Object.assign(toast.style, {
                 position: "absolute",
                 bottom: "6px",
                 left: "50%",
-                width:"100px",
                 transform: "translateX(-50%)",
                 background: "rgba(0,0,0,0.85)",
                 padding: "6px 10px",
@@ -92,37 +51,43 @@
                 fontSize: "11px",
                 opacity: "0",
                 transition: "opacity 0.2s",
-                pointerEvents: "none"
+                pointerEvents: "none",
+                color:"white"
             });
 
-            const textContent = element.children[1].textContent
-            navigator.clipboard.writeText(textContent).catch(e=>{
-                toast.textContent='Error'
-            })
-
-            copyfilecontent.appendChild(toast);
+            navigator.clipboard.writeText(`${svg.parentElement.parentElement.nextSibling.textContent}`).catch(err=>{
+              toast.textContent='Failed to copy'
+              toast.style.color='red'
+            });
+            copytextbutton.parentElement.appendChild(toast);
             requestAnimationFrame(() => toast.style.opacity = "1");
             setTimeout(() => {
                 toast.style.opacity = "0";
                 setTimeout(() => toast.remove(), 200);
             }, 2000);
-        }
+       }
 
-        if(!element.dataset.copyfilecontent){
-            element.firstChild.appendChild(copyfilecontent)
-            element.dataset.copyfilecontent=true
-        }
-    });
-
-    const pres = document.querySelectorAll(`pre[class='d_flex ov_auto scr-bar-w_thin flex-d_column']`)
-    pres.forEach(pre=>{
-      if(!pre.dataset.copyfilecontentslistener){
-        pre.addEventListener('contextmenu',(e)=>{
-          addContextMenuButton(e)
+       svg.parentElement.parentElement.appendChild(copytextbutton)
+       svg.parentElement.parentElement.parentElement.addEventListener('contextmenu',(e)=>{
+          const replybutton = document.querySelector(`a:has(svg>path[d='M10 9V5l-7 7 7 7v-4.1c5 0 8.5 1.6 11 5.1-1-5-4-10-11-11'])`)
+          if(replybutton){
+            addContextMenuButton(replybutton,svg)
+          }else{
+            const interval = setInterval(() => {
+              const replybutton = document.querySelector(`a:has(svg>path[d='M10 9V5l-7 7 7 7v-4.1c5 0 8.5 1.6 11 5.1-1-5-4-10-11-11'])`)
+              if(replybutton){
+                addContextMenuButton(replybutton,svg)
+                clearInterval(interval)
+              }
+            }, 1);
+          }
         });
-        pre.dataset.copyfilecontentslistener=true
-      }
     });
+
+    const replybutton = document.querySelector(`a:has(svg>path[d='M10 9V5l-7 7 7 7v-4.1c5 0 8.5 1.6 11 5.1-1-5-4-10-11-11'])`)
+    if(!replybutton&&document.body.style.getPropertyValue('overflow')){
+      document.body.style.removeProperty('overflow')
+    }
   }
 
   const observer = new MutationObserver(() => {
